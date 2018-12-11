f22be7cd8fb4fc8f4c29f60175819f16e2c51a2b
Roi C <roicou@gmail.com>
2018-12-11 19:30:21 +0100

    Prueba a ver si va

--- pacificrom.js
+++ pacificrom.js
@@ -58,7 +58,7 @@ function ColocarPjAleatorio(tablero, personaje) {
 
 
 /**
- * @description Da un número entero entre 0 y la coordenada máxima que tengamos (X o Y)
+ * @description Da un número entero entre 0 y la coordenada máxima que tengamos (X o Y) //Git prueba
  * @param {number} maximo Coordenada máxima 
  * @returns Número
  */
